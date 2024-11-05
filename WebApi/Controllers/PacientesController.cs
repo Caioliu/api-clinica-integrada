f35618a31eb46944380095088debc09b4891fc3a
Caioliu <caioarthur_@hotmail.com>
2024-11-05 19:59:55 -0300

    pacientes commands

--- WebApi/Controllers/PacientesController.cs
+++ WebApi/Controllers/PacientesController.cs
@@ -1,5 +1,7 @@
 ﻿using Application.DTOs;
 using Application.Handlers.Pacientes.Commands.Create;
+using Application.Handlers.Pacientes.Commands.Delete;
+using Application.Handlers.Pacientes.Commands.Update;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +11,29 @@ namespace WebApi.Controllers
     [ApiController]
     public class PacientesController : ApiControllerBase
     {
-        [Authorize(Roles = "atendente")]
+        //[Authorize(Roles = "atendente")]
         [HttpPost]
         public async Task<ActionResult<PacienteDto>> Create([FromBody] CreatePacienteCommand command) {
             return await Mediator.Send(command);
+
+        }
+
+        //[Authorize(Roles = "atendente")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult<PacienteDto>> Update(Guid id, [FromBody] UpdatePacienteCommand command) {
+            command.Id = id;
+            return await Mediator.Send(command);
+
+        }
+
+        //[Authorize(Roles = "atendente")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<string>> Delete(Guid id) {
+            var result = await Mediator.Send(new DeletePacienteCommand { Id = id });
+            if (result.Succeeded) {
+                return Ok(result.Data);
+            }
+            return BadRequest();
         }
     }
 }
