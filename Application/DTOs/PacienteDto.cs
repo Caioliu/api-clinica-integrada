73b8ce0adc1207ce023cb513b3bc04021ff0d43d
Caioliu <caioarthur_@hotmail.com>
2024-11-05 18:08:15 -0300

    modelando entidade paciente, backup antes da migração

---
+++ Application/DTOs/PacienteDto.cs
@@ -0,0 +1,16 @@
+﻿using Application.Mappings;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.DTOs
+{
+    public class PacienteDto : IMapFrom<Paciente>
+    {
+        public string Nome { get; set; }
+
+        public void Mapping(Profile profile) {
+            profile.CreateMap<Paciente, PacienteDto>()
+                .ForMember(d => d.Nome, opt => opt.MapFrom(s => s.Nome));
+        }
+    }
+}
---
+++ Application/Handlers/Pacientes/Commands/Create/CreatePacienteCommand.cs
@@ -0,0 +1,45 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Handlers.Pacientes.Commands.Create
+{
+    public class CreatePacienteCommand : PacienteCommand, IRequest<PacienteDto>
+    {
+
+    }
+
+    public class CreatePacienteCommandHandler : IRequestHandler<CreatePacienteCommand, PacienteDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CreatePacienteCommandHandler(IApplicationDbContext context,
+            IMapper mapper
+            ) 
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<PacienteDto> Handle(CreatePacienteCommand request, CancellationToken cancellationToken) {
+            try {
+                var entity = new Paciente {
+                    Nome = request.Nome
+                };
+
+                await _context.Pacientes.AddAsync(entity, cancellationToken);
+                //var historico = entity.GerarHistoricoPersonalizado("Registro Incluído", TipoAcaoHistorico.Incluido, _currentUserService.CurrentUser.Id);
+                //await _context.Historicos.AddAsync(historico, cancellationToken);
+                await _context.SaveChangesAsync();
+                return _mapper.Map<PacienteDto>(entity);
+            } catch (Exception ex) {
+                await _context.RollBack();
+                throw;
+            }
+
+        }
+    }
+}
