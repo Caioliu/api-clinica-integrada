abfecdbf64efb8d0bf65d991d0b8045680f82ce9
Caioliu <caioarthur_@hotmail.com>
2024-11-15 15:11:59 -0300

    Salas: Post e GetAll, Incluindo migração

--- Infrastructure/Migrations/20241115180843_Agendamento_Consulta_Sala.Designer.cs
+++ Infrastructure/Migrations/20241115180843_Agendamento_Consulta_Sala.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241115180843_Agendamento_Consulta_Sala")]
+    partial class Agendamento_Consulta_Sala
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
