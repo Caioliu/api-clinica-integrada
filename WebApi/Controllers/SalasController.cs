033255f3f645facd6140c8acfacb4844e8d09e10
Caioliu <caioarthur_@hotmail.com>
2024-11-15 15:11:59 -0300

    Salas: Post e GetAll, Incluindo migração

---
+++ WebApi/Controllers/SalasController.cs
@@ -0,0 +1,28 @@
+﻿using Application.DTOs;
+using Application.Handlers.Salas.Commands.Create;
+using Application.Handlers.Salas.Queries.GetSalas;
+using Application.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/salas")]
+    [ApiController]
+    public class SalasController : ApiControllerBase
+    {
+        //[Authorize(Roles = "atendente")]
+        [HttpGet]
+        public async Task<ActionResult> Get([FromQuery] GetSalasQuery query) {
+            return Ok(await Mediator.Send(query));
+        }
+
+        //[Authorize(Roles = "atendente")]
+        [HttpPost]
+        public async Task<ServiceResult> Create([FromBody] CreateSalaCommand command) {
+            return await Mediator.Send(command);
+
+        }
+    }
+}
