e34ef501b2ec2b984d9a0e480a15a72925c2dd0e
Caioliu <caioarthur_@hotmail.com>
2024-12-08 14:37:41 -0300

    backup a fim de evitar desastres pois estou bebado mexendo com migrações

--- Application/DTOs/AgendamentoDTO.cs
+++ Application/DTOs/AgendamentoDTO.cs
@@ -8,21 +8,28 @@ namespace Application.DTOs
     public class AgendamentoDTO : IMapFrom<Agendamento>
     {
         public Guid Id { get; set; }
-        public DateTime DataHora { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataHoraInicio { get; set; }
+        public DateTime DataHoraFim { get; set; }
         public string Tipo { get; set; }
         public string Status { get; set; }
+        public string StatusConsulta { get; set; }
         public Guid PacienteId { get; set; }
+        public string Sala { get; set; }
         public Guid? SalaId { get; set; }
         public Guid? ConsultaId { get; set; }
 
         public void Mapping(MappingProfile profile) {
             profile.CreateMap<Agendamento, AgendamentoDTO>()
                 .ForMember(d => d.Id, opt => opt.MapFrom(s => s.Id))
-                .ForMember(d => d.DataHora, opt => opt.MapFrom(s => s.DataHoraInicio))
+                .ForMember(d => d.DataHoraInicio, opt => opt.MapFrom(s => s.DataHoraInicio))
+                .ForMember(d => d.DataHoraFim, opt => opt.MapFrom(s => s.DataHoraFim))
                 .ForMember(d => d.Tipo, opt => opt.MapFrom(s => s.Tipo))
                 .ForMember(d => d.Status, opt => opt.MapFrom(s => s.Status))
+                .ForMember(d => d.StatusConsulta, opt => opt.MapFrom(s => s.Consulta.Status))
                 .ForMember(d => d.PacienteId, opt => opt.MapFrom(s => s.PacienteId))
-                //.ForMember(d => d.EquipeId, opt => opt.MapFrom(s => s.EquipeId))
+                .ForMember(d => d.Nome, opt => opt.MapFrom(s => s.Paciente.Nome))
+                .ForMember(d => d.Sala, opt => opt.MapFrom(s => s.Sala.Nome))
                 .ForMember(d => d.SalaId, opt => opt.MapFrom(s => s.SalaId))
                 .ForMember(d => d.ConsultaId, opt => opt.MapFrom(s => s.ConsultaId));
         }
--- Domain/Entities/Equipe.cs
+++ Domain/Entities/Equipe.cs
@@ -8,7 +8,6 @@ namespace Domain.Entities
         public Guid Id { get; set; } = Guid.NewGuid();
         public Especialidade Especialidade { get; set; }
 
-
         //Relacionamentos
         public IList<EquipeProfissional> Profissionais { get; set; }
         public IList<Consulta> Consultas { get; set; }
